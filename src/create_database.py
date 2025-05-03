5a2f6e1561b27ee9de831bb070175aff335fb730
RuslanZaripov <zaripovruslan864@gmail.com>
2025-05-03 10:24:43 +0300

    implement clustering script

--- src/create_database.py
+++ src/create_database.py
@@ -1,25 +1,28 @@
 import os
 import argparse
+import textwrap
 import pandas as pd
 from logger import Logger
-from cassandra.cluster import Cluster
+from clickhouse_driver import Client
+from dotenv import load_dotenv
 
 
 class CassandraDatabaseCreator:
     def __init__(self):
-        self.SHOW_LOG = True
-        self.logger = Logger(self.SHOW_LOG)
+        self.logger = Logger(show=True)
         self.log = self.logger.get_logger(__name__)
         
         self.args = self._parse_arguments()
         
-        self.KEYSPACE = os.environ["CASSANDRA_KEYSPACE"]
-        self.IP_ADDRESS = os.environ["CASSANDRA_IP_ADDRESS"]
-        self.PORT = os.environ["CASSANDRA_PORT"]
+        load_dotenv()
+        
+        self.PORT = "9000"
+        self.IP_ADDRESS = os.environ.get("CLICKHOUSE_IP_ADDRESS", "localhost")
+        self.USER = os.environ.get("CLICKHOUSE_USER", "default")
+        self.PASSWORD = os.environ.get("CLICKHOUSE_PASSWORD", "")
         self.timeout = 60
         
-        self.cluster = None
-        self.session = None
+        self.client = None
 
     def _parse_arguments(self):
         parser = argparse.ArgumentParser(description="Create Database")
@@ -37,70 +40,68 @@ class CassandraDatabaseCreator:
             help="Delimiter used in the CSV file (default: autodetect)")
         return parser.parse_args()
 
-    def _get_cassandra_session(self):
-        contact_points = self.IP_ADDRESS.split(',')
-        self.log.info(f"{contact_points=} {self.PORT=}")
-        cluster = Cluster(
-            contact_points=contact_points,
-            port=self.PORT,
-            connect_timeout=self.timeout,
+    def _get_clickhouse_client(self):
+        self.log.info(f"Connecting to ClickHouse at {self.IP_ADDRESS}:{self.PORT}")
+        client = Client(
+            host=self.IP_ADDRESS,
+            port=int(self.PORT),
+            user=self.USER,
+            password=self.PASSWORD,
+            connect_timeout=self.timeout
         )
-        session = cluster.connect()
-        return cluster, session
+        return client
 
-    def _create_keyspace(self):
-        self.log.info(f"Dropping keyspace {self.KEYSPACE} if it exists...")
-        self.session.execute(f"DROP KEYSPACE IF EXISTS {self.KEYSPACE}", timeout=self.timeout)
-
-        self.log.info(f"Creating keyspace {self.KEYSPACE} if not exists...")
-        self.session.execute("""
-            CREATE KEYSPACE IF NOT EXISTS %s
-            WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '1' }
-            """ % self.KEYSPACE)
-
-        self.session = self.cluster.connect(self.KEYSPACE, wait_for_all_pools=True)
-
-    def _get_column_definitions(self):
-        csv_file_path = self.args.csv_path
-        columns = pd.read_csv(csv_file_path, delimiter=self.args.delimiter, nrows=1).columns.tolist()
-        
-        column_definitions = []
-        for col in columns:
+    def _infer_column_types(self, sample_data):
+        column_types = []
+        for col, dtype in sample_data.dtypes.items():
+            ch_type = 'String'
+            col_name = col.replace('-', '_')
             if col == 'code' or col == 'IndexColumn':
-                col_type = 'text PRIMARY KEY'
+                column_types.append(f"{col_name} {ch_type}")
             else:
-                col_type = 'text'
-            column_definitions.append(f"{col.replace('-', '_')} {col_type}")
-        return column_definitions
-
+                column_types.append(f"{col_name} Nullable({ch_type})")
+        return column_types
+    
     def _create_table(self):
-        column_definitions = self._get_column_definitions()
+        sample_data = pd.read_csv(
+            self.args.csv_path, 
+            delimiter=self.args.delimiter, 
+            nrows=2,
+            engine='python'
+        )
+        
+        column_definitions = self._infer_column_types(sample_data)
         
         self.log.info(f"Dropping table {self.args.table_name} if it exists...")
-        self.session.execute(f"DROP TABLE IF EXISTS {self.args.table_name}", timeout=self.timeout)
+        self.client.execute(f"DROP TABLE IF EXISTS {self.args.table_name}")
             
-        self.log.info(f"Creating table {self.args.table_name} if it not exists...")
-        definitions_str = '\t' + ',\n\t'.join(column_definitions)
+        self.log.info(f"Creating table {self.args.table_name}...")
+        definitions_str = '\t\t' + ',\n\t\t'.join(column_definitions)
+        
         table_creation_command = f"""
-        CREATE TABLE IF NOT EXISTS {self.args.table_name} (
+        CREATE TABLE {self.args.table_name} (
         {definitions_str}
         )
+        ENGINE = MergeTree()
+        PRIMARY KEY (code)
         """
-        print(table_creation_command)
-        self.session.execute(table_creation_command, timeout=self.timeout)
+        
+        self.log.debug(f"Creating table with command:\n{table_creation_command}")
+        self.client.execute(table_creation_command)
+
 
     def run(self):
         try:
-            self.cluster, self.session = self._get_cassandra_session()
-            self._create_keyspace()
+            self.client = self._get_clickhouse_client()
             self._create_table()
+            self.log.info("Database and table created successfully!")
             
         except Exception as e:
             self.log.error(f"Error occurred while creating database: {e}")
 
         finally:
-            if self.cluster is not None:
-                self.cluster.shutdown()
+            if self.client is not None:
+                self.client.disconnect()
 
 
 if __name__ == "__main__":
